a2985dc909a8f8de2eb076d7687af681c32f7fe7
Andrew Sanchez <inbox.asanchez@gmail.com>
2020-07-14 13:36:35 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='q2doc',
+    packages=find_packages(),
+    license='BSD-3-Clause',
+    url='https://qiime2.org',
+    include_package_data=True,
+)
