324ceffd3bb6c45697ef78cf0e370bf11702c986
Andrew Sanchez <inbox.asanchez@gmail.com>
2021-02-11 18:20:00 -0700

    save work

--- q2doc/tests/test_q2_doc.py
+++ q2doc/tests/test_q2_doc.py
@@ -1,23 +1,57 @@
+import os
 import pytest
 
+import qiime2
+import qiime2.sdk.usage as usage
+from docutils import nodes
+import itertools
+from qiime2.plugins import ArtifactAPIUsage
+from q2cli.core.usage import CLIUsage
 
-@pytest.mark.sphinx(buildername='text', testroot="ext-usage", freshenv=True)
-def test_usage_text(app):
-    app.build()
-    assert app.statuscode == 0
-    assert 'q2doc.usage' in app.extensions
-    build_result = (app.outdir / 'cutadapt.txt').text()
-    exp = """
-   from qiime2.plugins.cutadapt.methods import demux_single
+from q2doc.usage.usage import records_to_nodes
 
-   barcodes = metadata.get_column('barcodes')
 
-   demultiplexed_seqs, untrimmed = demux_single(
-       seqs=data,
-       barcodes=barcodes,
-       error_rate=0,
-   )"""
-    assert exp in build_result
+def data_factory():
+    return qiime2.Artifact.import_data('MultiplexedSingleEndBarcodeInSequence',
+                                       'roots/test-ext-usage/data/forward.fastq.gz')
+
+
+def metadata_factory():
+    return qiime2.Metadata.load('roots/test-ext-usage/data/metadata.tsv')
+
+
+def example_init_data(use):
+    use.init_data('data', data_factory)
+    use.init_metadata('metadata', metadata_factory)
+    records = use._scope.records.values()
+    return records
+
+
+def test_records_to_nodes_execution():
+    use = usage.ExecutionUsage()
+    result = records_to_nodes(use, [])
+    assert result == []
+    records = example_init_data(use)
+    result = records_to_nodes(use, records)
+    exp = itertools.zip_longest(result, [], fillvalue=nodes.Node)
+    assert all(itertools.starmap(isinstance, exp))
+    assert len(result) == 4
+    assert result
+
+
+@pytest.mark.parametrize("use", [CLIUsage(), ArtifactAPIUsage()])
+def test_records_to_nodes_cli(use):
+    result = records_to_nodes(use, [])
+    assert result == []
+    example_init_data(use)
+    records = use._scope.records.values()
+    result = records_to_nodes(use, records)
+    assert not result
+
+
+def test_get_new_records():
+    pass
+
 
 @pytest.mark.sphinx(buildername='html', testroot="ext-usage", freshenv=True)
 def test_usage_html(app):
--- q2doc/usage/usage.py
+++ q2doc/usage/usage.py
@@ -1,6 +1,9 @@
 import ast
 import os
 import operator
+import functools
+from enum import Enum
+from typing import List, Type
 
 from docutils import nodes
 from docutils.parsers.rst import Directive
@@ -35,40 +38,80 @@ class UsageDirective(Directive):
 def process_usage_blocks(app, doctree, fromdocname):
     env = app.builder.env
     os.chdir(env.srcdir)
-    drivers = (ArtifactAPIUsage(), CLIUsage())
-    driver_nodes = []
-    nodes_ = []
-    for use in drivers:
+    all_nodes = {ix: [] for ix, _ in enumerate(env.usage_blocks)}
+    for use in MetaUsage:
+        use = use.value
+        processed_records = []
         for ix, (node, code) in enumerate(zip(doctree.traverse(UsageBlock), env.usage_blocks)):
+            # execute code in the block
             code = code["code"]
             tree = ast.parse(code)
             source = compile(tree, filename="<ast>", mode="exec")
-            processed_records = set()
+            # TODO: validate the ast
             exec(source)
-            new_records = get_new_records(use._scope.records, processed_records)
-            for record in new_records:
-                record_source = record.source
-                if record_source == "init_data":
-                    pass
-                elif record_source == "init_metadata":
-                    pass
-                elif record_source == "get_metadata_column":
-                    pass
+            new_records = get_new_records(use, processed_records)
+            nodes_ = records_to_nodes(use, new_records)
+            if nodes_:
+                all_nodes[ix].extend(nodes_)
             if new_records:
                 refs = [i.ref for i in new_records]
-                processed_records.update(refs)
-        example = use.render()
-        example_node = nodes.literal_block(example, example)
-        nodes_.append(example_node)
-    driver_nodes = [*nodes_]
-    for node_list, tmp_node in zip(driver_nodes, doctree.traverse(UsageBlock)):
-        tmp_node.replace_self(node_list)
+                processed_records.extend(refs)
+    for node_list, tmp_node in zip(all_nodes.values(), doctree.traverse(UsageBlock)):
+        if node_list:
+            tmp_node.replace_self(node_list)
 
 
-def get_new_records(records, processed_records):
-    new_records = records.keys() - processed_records
-    records = operator.itemgetter(*new_records)(records) if new_records else {}
+@functools.singledispatch
+def records_to_nodes(use, records) -> List[Type[nodes.Node]]:
+    return [nodes.Node]
+
+
+@records_to_nodes.register(usage.ExecutionUsage)
+def _(use, records):
+    nodes_ = []
+    for record in records:
+        source = record.source
+        data = record.result
+        record_type = type(record.result)
+        if source == "init_data":
+            nodes_.append(nodes.title(text="Data"))
+            data = str(data.type)
+        elif source == "init_metadata":
+            nodes_.append(nodes.title(text="Metadata"))
+            data = str(data.to_dataframe().head())
+        else:
+            return []
+        nodes_.append(nodes.literal_block(data, data))
+    return nodes_
+
+
+@records_to_nodes.register(CLIUsage)
+@records_to_nodes.register(ArtifactAPIUsage)
+def _(use, records):
+    nodes_ = []
+    for record in records:
+        source = record.source
+        if source == "action":
+            result = use.render()
+            title = "Action"
+            nodes_.append(nodes.title(text=title))
+            nodes_.append(nodes.literal_block(result, result))
+            break
+    return nodes_
+
+
+def get_new_records(use, processed_records):
+    records = use._scope.records
+    # Use a list to preserve the order
+    new_records = [k for k in records.keys() if k not in processed_records]
+    records = operator.itemgetter(*new_records)(records) if new_records else tuple()
     return records
+
+
+class MetaUsage(Enum):
+    execution = usage.ExecutionUsage()
+    cli = CLIUsage()
+    artifact_api = ArtifactAPIUsage()
 
 
 def setup(app):
