a5c1f29b1764b2f082091e315f920ebd0b3a0f30
Andrew Sanchez <inbox.asanchez@gmail.com>
2021-03-26 14:47:12 -0700

    noop helper.

--- q2doc/usage/__init__.py
+++ q2doc/usage/__init__.py
@@ -5,13 +5,13 @@ from q2doc.usage.nodes import UsageNode, UsageExampleNode, UsageDataNode, UsageM
 
 def setup(app):
     app.add_directive("usage", UsageDirective)
-    app.add_node(UsageNode, html=(lambda *_: None, lambda *_: None))
-    app.add_node(
-        UsageExampleNode, html=(lambda *_: None, UsageExampleNode.depart)
-    )
-    app.add_node(UsageDataNode, html=(lambda *_: None, UsageDataNode.depart))
-    app.add_node(
-        UsageMetadataNode, html=(lambda *_: None, UsageMetadataNode.depart)
-    )
+    app.add_node(UsageNode, html=(_, _))
+    app.add_node(UsageExampleNode, html=(_, UsageExampleNode.depart))
+    app.add_node(UsageDataNode, html=(_, UsageDataNode.depart))
+    app.add_node(UsageMetadataNode, html=(_, UsageMetadataNode.depart))
     app.add_config_value('factory', False, 'html')
     app.connect("doctree-resolved", process_usage_blocks)
+
+
+def _(*_):
+    pass
