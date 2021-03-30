cec2ccd3f1b5e166f329dfc61183cd6cbb64524b
Andrew Sanchez <inbox.asanchez@gmail.com>
2021-03-30 13:08:28 -0700

    Constrain factory spec to choices.

--- q2doc/usage/directive.py
+++ q2doc/usage/directive.py
@@ -4,19 +4,25 @@ from sphinx.util.docutils import SphinxDirective
 from q2doc.usage.nodes import UsageNode
 
 
+def factory_spec(name):
+    return directives.choice(name, ('data', 'metadata'))
+
+
 class UsageDirective(SphinxDirective):
     has_content = True
-    option_spec = {'factory': docutils.parsers.rst.directives.flag}
+    option_spec = {
+        'factory': factory_spec,
         'name': str
+    }
+    # TODO Validate the spec
+    #  assert v in factory.values()
 
     def run(self):
         code = "\n".join(self.content)
         env = self.state.document.settings.env
         if not hasattr(env, "usage_blocks"):
             env.usage_blocks = []
-        env.usage_blocks.append({"code": code})
-        factory = "factory" in self.options
-        return [UsageNode(factory=factory)]
+        factory = self.options.get('factory')
         name = self.options.get('name')
         nodes = [UsageNode(factory=factory, name=name)]
         env.usage_blocks.append(
