83a07737e9d567fb6b25e268461d1ea1ac5aed83
Andrew Sanchez <inbox.asanchez@gmail.com>
2021-04-01 13:19:50 -0700

    This wasn't necessary.

--- q2doc/tests/conftest.py
+++ q2doc/tests/conftest.py
@@ -11,18 +11,3 @@ pytest_plugins = 'sphinx.testing.fixtures'
 def rootdir():
     """Sets root folder for sphinx unit tests."""
     return path(__file__).parent.abspath() / 'roots'
-
-
-def _initialize_test_directory(session):
-    if 'SPHINX_TEST_TEMPDIR' in os.environ:
-        tempdir = os.path.abspath(os.getenv('SPHINX_TEST_TEMPDIR'))
-        print('Temporary files will be placed in %s.' % tempdir)
-
-        if os.path.exists(tempdir):
-            shutil.rmtree(tempdir)
-
-        os.makedirs(tempdir)
-
-
-def pytest_sessionstart(session):
-    _initialize_test_directory(session)
