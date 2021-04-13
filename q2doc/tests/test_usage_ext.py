2826b244b00e3ed94c6711ff7bebbbb4b2c21e36
Andrew Sanchez <inbox.asanchez@gmail.com>
2021-04-12 17:15:23 -0700

    Test examples.

--- q2doc/tests/test_usage_ext.py
+++ q2doc/tests/test_usage_ext.py
@@ -137,6 +137,14 @@ def test_cutadapt_html(app, file_regression):
     file_regression.check(build_result.read_text(), extension=".html")
 
 
+@pytest.mark.sphinx(buildername='singlehtml', testroot="examples", freshenv=True)
+def test_examples_html(app, file_regression):
+    app.build()
+    assert app.statuscode == 0
+    build_result = app.outdir / 'examples' / 'index.html'
+    file_regression.check(build_result.read_text(), extension=".html")
+
+
 @pytest.mark.sphinx(buildername='html', testroot="q2doc", freshenv=True)
 def test_command_block_html(app):
     app.build()
