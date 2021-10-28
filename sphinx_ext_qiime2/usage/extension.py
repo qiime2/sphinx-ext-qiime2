36aaceeb14d8f18a0f693e78e336a307aee138cd
Matthew Dillon <matthewrdillon@gmail.com>
2021-10-28 13:32:18 -0700

    whoa

--- sphinx_ext_qiime2/usage/extension.py
+++ sphinx_ext_qiime2/usage/extension.py
@@ -1,13 +1,37 @@
+from io import StringIO
+from contextlib import redirect_stdout
+
 import docutils.parsers.rst.directives
+
+
+from qiime2.plugins import ArtifactAPIUsage
 
 
 class UsageDirective(docutils.parsers.rst.Directive):
     has_content = True
 
     def run(self):
-        content = '\n'.join(self.content)
-        node = docutils.nodes.literal_block(content, content)
-        return [node]
+        use = ArtifactAPIUsage()
+
+        cmd = '\n'.join(self.content)
+
+        cmd_node = docutils.nodes.literal_block(cmd, cmd)
+
+        buf = StringIO()
+        with redirect_stdout(buf):
+            locals()['use'] = use
+            # JUST DO IT
+            exec(cmd, globals(), locals())
+        stdout = buf.getvalue()
+
+        stdout_node = docutils.nodes.literal_block(stdout, stdout)
+
+        rendered = use.render()
+
+        rendered_usage_node = docutils.nodes.literal_block(rendered, rendered)
+
+        return [cmd_node, stdout_node, rendered_usage_node]
+
 
 def setup(app):
     app.add_directive('usage', UsageDirective)
