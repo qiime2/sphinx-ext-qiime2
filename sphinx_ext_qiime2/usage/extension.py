0e88364b193b1f29f69ef8570fa11f35caecff7b
Matthew Dillon <matthewrdillon@gmail.com>
2021-10-28 14:47:02 -0700

    wip

--- sphinx_ext_qiime2/usage/extension.py
+++ sphinx_ext_qiime2/usage/extension.py
@@ -5,32 +5,39 @@ import docutils.parsers.rst.directives
 
 
 from qiime2.plugins import ArtifactAPIUsage
+from qiime2.sdk import PluginManager
+from qiime2.sdk.usage import ExecutionUsage
 
 
 class UsageDirective(docutils.parsers.rst.Directive):
     has_content = True
 
     def run(self):
-        use = ArtifactAPIUsage()
+        PluginManager()
+
+        uses = ArtifactAPIUsage()
+        exc = ExecutionUsage()
 
         cmd = '\n'.join(self.content)
 
-        cmd_node = docutils.nodes.literal_block(cmd, cmd)
-
         buf = StringIO()
         with redirect_stdout(buf):
-            locals()['use'] = use
-            # JUST DO IT
-            exec(cmd, globals(), locals())
+            exec(cmd, dict(), {'use': use})
+        exec(cmd, {'use': exc})
+
         stdout = buf.getvalue()
 
-        stdout_node = docutils.nodes.literal_block(stdout, stdout)
-
         rendered = use.render()
-
         rendered_usage_node = docutils.nodes.literal_block(rendered, rendered)
 
-        return [cmd_node, stdout_node, rendered_usage_node]
+        outputs = '\n'.join(exc.records.keys())
+        outputs_node = docutils.nodes.literal_block(outputs, outputs)
+
+        # BREAK IN CASE OF EMERGENCY
+        # cmd_node = docutils.nodes.literal_block(cmd, cmd)
+        # stdout_node = docutils.nodes.literal_block(stdout, stdout)
+        # return [cmd_node, stdout_node, rendered_usage_node]
+        return [rendered_usage_node, outputs_node]
 
 
 def setup(app):
