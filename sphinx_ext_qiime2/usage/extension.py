c70df30d86dc4c2b104aaef034559d25c16e78d5
Matthew Dillon <matthewrdillon@gmail.com>
2021-10-28 12:51:47 -0700

    squash

--- sphinx_ext_qiime2/usage/extension.py
+++ sphinx_ext_qiime2/usage/extension.py
@@ -1,2 +1,14 @@
+import docutils.parsers.rst.directives
+
+
+class UsageDirective(docutils.parsers.rst.Directive):
+    has_content = True
+
+    def run(self):
+        content = '\n'.join(self.content)
+        node = docutils.nodes.literal_block(content, content)
+        return [node]
+
 def setup(app):
+    app.add_directive('usage', UsageDirective)
     return {'version': '0.0.1'}
