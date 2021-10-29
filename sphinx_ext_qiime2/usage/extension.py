51c7298766b9518854100320f542c8b30a0b07d1
Matthew Dillon <matthewrdillon@gmail.com>
2021-10-28 21:28:19 -0700

    wip

--- sphinx_ext_qiime2/usage/extension.py
+++ sphinx_ext_qiime2/usage/extension.py
@@ -2,33 +2,78 @@ import docutils.parsers.rst.directives
 
 from qiime2.plugins import ArtifactAPIUsage
 from qiime2.sdk import PluginManager
-from qiime2.sdk.usage import ExecutionUsage
+from qiime2.sdk.usage import Usage, UsageVariable
 from q2cli.core.usage import CLIUsageFormatter
 
 
-def render_artifact_api(use):
-    rendered =  use.render()
-    use.recorder = []
-    return '# artifact api:\n\n%s' % rendered
+class SphinxExecUsageVariable(UsageVariable):
+    def assert_has_line_matching(self, path, expression):
+        data = self.value
+
+        hits = sorted(data._archiver.data_dir.glob(path))
+        if len(hits) != 1:
+            raise ValueError('Value provided for path (%s) did not produce '
+                             'exactly one hit: %s' % (path, hits))
+
+        target = hits[0].read_text()
+        match = re.search(expression, target, flags=re.MULTILINE)
+        if match is None:
+            raise AssertionError('Expression %r not found in %s.' %
+                                 (expression, path))
+
+    def assert_output_type(self, semantic_type):
+        data = self.value
+
+        if str(data.type) != str(semantic_type):
+            raise AssertionError("Output %r has type %s, which does not match"
+                                 " expected output type of %s"
+                                 % (self.name, data.type, semantic_type))
 
 
-def render_cli(use):
-    rendered = '\n'.join(use.lines)
-    use.lines = []
-    return '# q2cli:\n\n%s' % rendered
+class SphinxExecUsage(Usage):
+    def __init__(self, asynchronous=False):
+        super().__init__(asynchronous)
+        self.recorder = {}
 
+    def variable_factory(self, name, factory, var_type):
+        return SphinxExecUsageVariable(
+            name,
+            factory,
+            var_type,
+            self,
+        )
 
-def render_exc(use):
-    rendered = '\n'.join(use.records.keys())
-    use.records = {}
-    return 'executed:\n\n%s' % rendered
+    def _add_record(self, variable):
+        variable.execute()
+        self.recorder[variable.name] = variable.value
+        return variable
 
+    def render(self, flush=True):
+        records = self.recorder
+        if flush:
+            self.recorder = {}
+        return records
 
-render = {
-    'artifact_api': render_artifact_api,
-    'cli': render_cli,
-    'exc': render_exc,
-}
+    def init_artifact(self, name, factory):
+        variable = super().init_artifact(name, factory)
+        return self._add_record(variable)
+
+    def init_metadata(self, name, factory):
+        variable = super().init_metadata(name, factory)
+        return self._add_record(variable)
+
+    def merge_metadata(self, name, *variables):
+        variable = super().merge_metadata(name, *variables)
+        return self._add_record(variable)
+
+    def get_metadata_column(self, name, column_name, variable):
+        variable = super().get_metadata_column(name, column_name, variable)
+        return self._add_record(variable)
+
+    def action(self, action, input_opts, output_opts):
+        variables = super().action(action, input_opts, output_opts)
+        [self._add_record(v) for v in variables]
+        return variables
 
 
 def setup_usage_drivers(app):
@@ -36,7 +81,7 @@ def setup_usage_drivers(app):
     app.contexts = {
         'artifact_api': {'use': ArtifactAPIUsage()},
         'cli': {'use': CLIUsageFormatter()},
-        'exc': {'use': ExecutionUsage()},
+        'exc': {'use': SphinxExecUsage()},
     }
 
 
@@ -51,9 +96,12 @@ class UsageDirective(docutils.parsers.rst.Directive):
 
         for driver_name, ctx in contexts.items():
             exec(cmd, ctx)
-            rendered = render[driver_name](ctx['use'])
-            rendered_usage_node = docutils.nodes.literal_block(rendered, rendered)
-            nodes.append(rendered_usage_node)
+            rendered = ctx['use'].render(flush=True)
+            if isinstance(rendered, dict):
+                rendered = '\n'.join('%s: %s' % (k, str(r)) for k, r in rendered.items())
+                nodes.append(docutils.nodes.literal_block(rendered, rendered))
+            else:
+                nodes.append(docutils.nodes.literal_block(rendered, rendered))
 
         return nodes
 
