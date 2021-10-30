cff8f296dadffd32f4ce456dd6db62052de59705
Matthew Dillon <matthewrdillon@gmail.com>
2021-10-29 22:32:57 -0700

    wip

--- sphinx_ext_qiime2/usage/assets/usage.js
+++ sphinx_ext_qiime2/usage/assets/usage.js
@@ -1,11 +1,29 @@
+KNOWN_INTERFACES = {
+  'artifact-usage': 'Artifact API',
+  'cli-usage': 'q2cli',
+};
+
 window.addEventListener('load', () => {
-  showInterface('cli-usage');
+  showInterface(Object.keys(KNOWN_INTERFACES)[0]);
+
+  const firstUsageBlock = document.getElementById('usage-0');
+  const interfaceSelect = document.createElement('select');
+  interfaceSelect.addEventListener('change', (e) => showInterface(e.target.value));
+
+  for (const [value, text] of Object.entries(KNOWN_INTERFACES)) {
+    const opt = document.createElement('option');
+    opt.value = value;
+    opt.text = text;
+    interfaceSelect.appendChild(opt);
+  };
+
+  firstUsageBlock.parentElement.querySelector('h1:first-of-type').after(interfaceSelect);
 });
 
 function showInterface(interfaceName) {
-  knownInterfaces = ['artifact-usage', 'cli-usage'];
-  if (!knownInterfaces.includes(interfaceName)) { return };
-  knownInterfaces.forEach((el) => {
+  const knownInterfaceKeys = Object.keys(KNOWN_INTERFACES);
+  if (!knownInterfaceKeys.includes(interfaceName)) { return };
+  knownInterfaceKeys.forEach((el) => {
     const hidden = interfaceName !== el;
     document.querySelectorAll(`.${el}`).forEach((el) => el.hidden = hidden);
   });
