c48a369cd55650dfe0fb30d5dd67aa13392ba6cf
Matthew Dillon <matthewrdillon@gmail.com>
2022-01-20 11:03:20 -0700

    bug: incomplete external-links extension

--- q2doc/external_links/__init__.py
+++ q2doc/external_links/__init__.py
@@ -6,7 +6,6 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+from .extension import setup
 
-def setup(app):
-    app.add_js_file('external-links.js')
-    return {'version': '0.0.1'}
+__all__ = ['setup']
